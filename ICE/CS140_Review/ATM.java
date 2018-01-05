5d27767998fbbcdf959c8953890070669fbdf718
cassilam <cassilam@cpp.edu>
2018-01-04 19:15:45 -0800

    added steps in code and TODO

--- ICE/CS140_Review/ATM.java
+++ ICE/CS140_Review/ATM.java
@@ -10,10 +10,10 @@ import java.io.*;
  * ATM- This program allows the user to check their balance, make a deposit, 
  * and withdraw money.
  *
- * 1.) Asks user to input the name of th efiule that contains their balance
+ * 1.) Asks user to input the name of the file that contains their balance
  * 2.) If the file does not exist, terminate the program.
  * 3.) If the file exists, ask the user whether they'd like to withdraw or 
- *     deposit money into thier account
+ *     deposit money into their account
  * 4.) If the user wants to withdraw, ensure that the amount they want to 
  *     withdraw is of equal or less value of their balance
  *
@@ -23,11 +23,14 @@ public class ATM
 {
    public static void main(String[] args) throws IOException 
    {
+      // Asks user to input the name of the file that contains their balance
       Scanner kb = new Scanner(System.in);
       System.out.print("Welcome! Please enter your filename: ");
       String filename = kb.nextLine();
       File file = new File(filename);
       Scanner inputFile = new Scanner(file);
+
+      // If the file does not exist, terminate the program.
       if (!file.exists())
       {
          System.out.print("File does not exist.");
@@ -37,9 +40,15 @@ public class ATM
       double balance = 0;
       while (inputFile.hasNextDouble())
          balance = inputFile.nextDouble();
+      
+      /*
+        If the file exists, ask the user whether they'd like to withdraw or 
+        deposit money into their account
+      */
  
       System.out.println("Select a transaction: ");
-      System.out.println("1 - Balance Inquiry\n2 - Withdraw Cash\n3 - Make A Deposit\n4 - Exit");
+      System.out.println("1 - Balance Inquiry\n2 - Withdraw Cash\n" + 
+                         "3 - Make A Deposit\n4 - Exit");
       int transaction = kb.nextInt();
       switch (transaction)
       {
@@ -50,4 +59,9 @@ public class ATM
             System.out.print("Deposit. Enter an amount: ");
       }            
    }
+   /*
+    * TODO: If the user wants to withdraw, ensure that the amount they want to 
+      withdraw is of equal or less value of their balance
+    */
+ 
 }
