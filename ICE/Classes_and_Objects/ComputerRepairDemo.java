eef1eea65dc510ebe78216372258ede45f5755e0
cassilam <rm.cassilam@gmail.com>
2018-01-18 13:05:20 -0800

    Added accessors (setters) for customer info fields

---
+++ ICE/Classes_and_Objects/ComputerRepairDemo.java
@@ -0,0 +1,14 @@
+/**
+ * This program demonstrates the Customer class.
+ */
+
+public class ComputerRepairDemo
+{
+	public static void main(String[] args)
+	{
+		// Create a customer object
+		Customer firstCustomer = 
+			new Customer("John Smith", "12345 Hollywood Dr", 1234567890);
+		
+	}
+}
--- ICE/Classes_and_Objects/Customer.java
+++ ICE/Classes_and_Objects/Customer.java
@@ -3,28 +3,54 @@
 // Winter 2018
 
 /**
- * The Customer class holds customer info.
+ * The Customer class stores customer info.
  */
 
 public class Customer
 {
-   private String customerName;
-   private String customerAddress;
-   private int customerNumber;
+   private String name;
+   private String streetAddress;
+   private int phoneNumber;
 
    /**
     * Constructor
-    * @param name The name of the customer
+    * @param n The name of the customer
     * @param address The customer's address
     * @param number The customer's phone number
     */
-   public Customer(String name, String address, int number)
+   public Customer(String n, String address, int number)
    {
-      customerName = name;
-      customerAddress = address;
-      customerNumber = number;
+      name = n;
+      streetAddress = address;
+      phoneNumber = number;
    }
+	
+	/**
+    * The setName method stores a value in the name field
+	 * @param n The value to store in name
+    */
+	public void setName(String n)
+	{
+		name = n;
+	}
 
+	/**
+	 * The setAddress method stores a value in the streetAddress field
+	 * @param address The value to store in streetAddress
+	 */
+	public void setAddress(String address)
+	{
+		streetAddress = address;
+	}
 
+	/**
+	 * The setNumber method stores a value in the phoneNumber field
+	 * @param number The value to store in phoneNumber
+	 */
+	public void setNumber(int number)
+	{
+		phoneNumber = number;
+	}
+	
 }
 
