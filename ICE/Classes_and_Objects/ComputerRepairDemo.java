150df1965eda314c12ffe91e8bf119c0f3d9444d
cassilam <rm.cassilam@gmail.com>
2018-01-18 13:28:52 -0800

    Modified the object parameters

--- ICE/Classes_and_Objects/ComputerRepairDemo.java
+++ ICE/Classes_and_Objects/ComputerRepairDemo.java
@@ -1,15 +1,26 @@
 /**
  * This program demonstrates the Customer class.
  */
+import java.util.Scanner;
 
 public class ComputerRepairDemo
 {
 	public static void main(String[] args)
 	{
+		// Ask user to enter their information (name, address, phone number)
+      Scanner kb = new Scanner(System.in);
+		System.out.println("Welcome! Please enter your information below.");
+		System.out.print("Name (first and last): ")
+		String name = kb.nextLine();
+		System.out.print("Street Address: ");
+		String streetAddress = kb.nextLine();		
+		System.out.print("Phone Number: ");
+		int phoneNumber = kb.nextInt();
+
 		// Create a customer object
 		Customer firstCustomer = 
-			new Customer("John Smith", "12345 Hollywood Dr", 1234567890);
-	
+			new Customer(name, streetAddress, phoneNumber);
+			
 	
 	}
 }
